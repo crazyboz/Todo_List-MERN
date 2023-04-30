d748ebacfd5680a707968dad9d67740c3d04c7ad
Dinesh <86416683+crazyboz@users.noreply.github.com>
2023-04-30 10:07:31 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import App from 'https://github.com/crazyboz/Todo_List-MERN/blob/406cf00f6069cb2773799e3c6a70a0c320906159/src/app.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
